0627ca84b62273614e66edee4e1f127ce7f66c92
托马斯 <gangzhucoll1990@live.com>
2021-03-17 21:10:29 +0800

    first commit

---
+++ BlazorBook/Data/BookInfo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorBook.Data
+{
+    public class BookInfo
+    {
+        public string Title { get; set; }
+        public DateTime PublictionDate { get; set; }
+        public string Author { get; set; }
+        public decimal Price { get; set; }
+        public string Type { get; set; }
+    }
+}
